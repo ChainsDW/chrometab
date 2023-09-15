108fdf9f503129dc9ca4c072baab43a73c389b73
wuyidi <wuyd@kingnet.com>
2023-09-15 10:29:32 +0800

    [feat] 搜索框和书签栏

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <Home></Home>
+  <div class="wallpaper">
+    <Layout style="background-color: rgba(0,0,0,0)">
+      <LayoutHeader :style="headerStyle"></LayoutHeader>
+      <LayoutContent>
+        <Row>
+          <Col :span="6">
+            <Left></Left>
+          </Col>
+          <Col :span="12">
+            <Home></Home>
+          </Col>
+          <Col :span="6"></Col>
+        </Row>
+      </LayoutContent>
+    </Layout>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import type { CSSProperties } from 'vue';
 import Home from './components/Home.vue';
-
+import Left from './components/left.vue';
+import {Layout, LayoutHeader, LayoutContent,Row, Col} from "ant-design-vue"
 export default defineComponent({
   name: 'App',
   components: {
-    Home
+    Home,
+    Layout,
+    LayoutHeader,
+    LayoutContent,
+    Row,
+    Col,
+    Left
+  },
+  setup(){
+    const headerStyle: CSSProperties = {
+      textAlign: 'center',
+      color: '#fff',
+      height: 64,
+      paddingInline: 50,
+      lineHeight: '64px',
+      backgroundColor: "rgba(0,0,0,0)",
+    };
+    return{headerStyle}
   }
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top:0;
+  left:0;
+  right: 0;
+  bottom: 0;
+  background-image: url("https://api.yimian.xyz/img?type=moe&size=1920x1080 ");
 }
+/*.wallpaper{*/
+/*  background-image: url("https://api.yimian.xyz/img?type=wallpaper");*/
+/*}*/
 </style>
---
+++ src/components/left.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="display: flex; justify-content: end; margin-right: 50px">
+    <Space direction="vertical">
+      <div class="cloud glass">
+      </div>
+    </Space>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {Space, Row, Col} from "ant-design-vue"
+</script>
+
+<style scoped>
+.glass{
+  color: white;
+  overflow: hidden;
+  background-color: rgba(0,0,0,0.2);
+  backdrop-filter: blur(10px)
+}
+.cloud{
+  width: 250px;
+  height: 100px;
+}
+/*.cloud:before{*/
+/* content: "";*/
+/* width: 250px;*/
+/* height: 250px;*/
+/* background: inherit;*/
+/* position: absolute;*/
+/* left: -25px;*/
+/* right: 0;*/
+/* top: -25px;*/
+/* bottom: 0;*/
+/* box-shadow: inset 0 0 0 200px rgba(59,59,59,0.2);*/
+/* filter: blur(10px);*/
+/*}*/
+</style>
